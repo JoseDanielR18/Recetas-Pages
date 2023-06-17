b43ae7d886b3fd59eed47bd25b3de1d8612239c6
JoseDanielR18 <rodriguezbedoyajose@gmail.com>
2023-06-17 14:32:38 -0500

    Update

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -6,7 +6,7 @@ export const NotFound = () => {
     return (
         <div className="fondo-not-found">
             <div className="centrado-found">
-                <img src="src/images/ramadan-fasting.png" alt="" />
+                <img src="images/ramadan-fasting.png" alt="" />
                 <h1>404 Not Found</h1>
                 <Link className="btn" to={"/recetas"}>Voler</Link>
             </div>
